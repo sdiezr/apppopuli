53c866ea05635aabcd0800462ed40fa820bde238
sdeezr00 <36732726+sdeezr00@users.noreply.github.com>
2022-05-25 11:44:14 +0200

    first commit
    Pestañas del footer funcionando correctamente. Pestaña de inicio seleccionada por defecto.

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app>
+    <v-footer
+      fixed
+      padless
+      color="white">
+      <v-card
+        color="#178649"
+        width="100%"
+        height="100%">
+        <v-tabs
+          v-model="activeTab"
+          background-color="#178649"
+          centered
+          fixed-tabs
+          dark
+          icons-and-text>    
+          <v-tab
+            v-for="icon in icons"
+            :key="icon"
+            :to="icon.ruta">
+            <div class="texto_iconos">
+              {{ icon.nombre }}
+            </div>
+            
+            <v-icon>
+              {{ icon.icono }}
+            </v-icon>
+          </v-tab>
+        </v-tabs>
+      </v-card>
+    </v-footer>
+
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    activeTab: 1,
+    icons: [
+      { nombre: 'AYUDA', icono: 'mdi-help', ruta: '/perro' },
+      { nombre: 'INICIO', icono: 'mdi-home', ruta: '/' },
+      { nombre: 'EXPLORAR', icono: 'mdi-map', ruta: '/perro' }
+    ]
+  })
+}
+</script>
+
+<style>
+
+  #app {
+    font-family: Helvetica;
+  }
+  
+  .texto_iconos {
+    margin-top: 5px;
+    font-size: 12px;
+    font-weight: bold
+  }
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+
+    <div class="icono_usuario">
+      <v-icon
+        large
+        color="#178649">
+        mdi-account-circle
+      </v-icon>
+    </div>
+
+    <div class="logotipo">
+      <img src="../assets/logo.png"
+        width="180" height="144" />
+    </div>
+
+    <div class="botones">
+      <v-btn class="white--text"
+        width="275"
+        height="50"
+        color="#ff5d55">
+        enviar informe
+      </v-btn>
+
+      <br><br>
+
+      <v-divider />
+
+      <br>
+
+      <v-btn class="white--text"
+        width="275"
+        height="50"
+        color="#178649">
+        iniciar sesión
+      </v-btn>
+    </div>
+
+  </v-app>
+</template>
+
+<style>
+
+  .icono_usuario {
+    text-align: left;
+    padding-top: 10px;
+    padding-left: 10px
+  }
+
+  .logotipo {
+    text-align: center
+  }
+
+  .botones {
+    text-align: center;
+    margin-top: 35px
+  }
+
+</style>
